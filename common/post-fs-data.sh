b29f154d6c871e68aeea7aac4a2d51f80b709617
Nikko Angelo <39809545+EmperorEye1993@users.noreply.github.com>
2019-02-25 16:16:41 +0800

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,6 +2,8 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
+setprop ro.vendor.qti.config.zram true
+write /proc/sys/vm/page-cluster 0
+write /sys/block/zram0/max_comp_streams 4
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
