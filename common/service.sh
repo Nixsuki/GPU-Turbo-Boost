7ff6f477ccceb2a1cbb8fa8bd2a883d375f47a14
Nikko Angelo <39809545+EmperorEye1993@users.noreply.github.com>
2019-02-25 16:29:00 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -3,8 +3,8 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 write /sys/class/kgsl/kgsl-3d0/devfreq/governor msm-adreno-tz
-	write /sys/module/cpu_boost/parameters/input_boost_freq 0
-	chmod 0664 /sys/module/sync/parameters/fsync_enabled
-	write /sys/module/sync/parameters/fsync_enabled N
+write /sys/module/cpu_boost/parameters/input_boost_freq 0
+chmod 0664 /sys/module/sync/parameters/fsync_enabled
+write /sys/module/sync/parameters/fsync_enabled N
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
