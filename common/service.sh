3868943ee79288cf504769aeb5cedca01082522f
Nikko Angelo <39809545+EmperorEye1993@users.noreply.github.com>
2019-06-24 03:37:28 +0800

    Update service.sh
    Updated GPU tweaks based on Spectrum.

--- common/service.sh
+++ common/service.sh
@@ -2,9 +2,13 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
-write /sys/class/kgsl/kgsl-3d0/devfreq/governor msm-adreno-tz
-write /sys/module/cpu_boost/parameters/input_boost_freq 0
-chmod 0664 /sys/module/sync/parameters/fsync_enabled
-write /sys/module/sync/parameters/fsync_enabled N
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/performance/go_hispeed_load 75
+write /sys/devices/system/cpu/cpufreq/performance/above_hispeed_delay 0
+write /sys/devices/system/cpu/cpufreq/performance/boost 1
+write /sys/module/msm_performance/parameters/touchboost 1
+write /sys/devices/system/cpu/cpufreq/performance/max_freq_hysteresis 1
+write /sys/devices/system/cpu/cpufreq/performance/align_windows 1
+write /sys/devices/soc/1c00000.qcom,kgsl-3d0/devfreq/1c00000.qcom,kgsl-3d0/governor performance
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
