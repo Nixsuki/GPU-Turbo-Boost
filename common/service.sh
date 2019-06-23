684c4753c334bb6c7fae358b550594ea62a7f905
Nikko Angelo <39809545+EmperorEye1993@users.noreply.github.com>
2019-06-24 03:52:07 +0800

    Update service.sh
    Update Spectrum based GPU Gaming Optimization.

--- common/service.sh
+++ common/service.sh
@@ -10,5 +10,11 @@ write /sys/module/msm_performance/parameters/touchboost 1
 write /sys/devices/system/cpu/cpufreq/performance/max_freq_hysteresis 1
 write /sys/devices/system/cpu/cpufreq/performance/align_windows 1
 write /sys/devices/soc/1c00000.qcom,kgsl-3d0/devfreq/1c00000.qcom,kgsl-3d0/governor performance
+write /sys/class/kgsl/kgsl-3d0/devfreq/governor msm-adreno-tz
+write /sys/module/adreno_idler/parameters/adreno_idler_active 0
+write /sys/module/lazyplug/parameters/nr_possible_cores 8
+write /dev/cpuset/foreground/cpus 0-2,4-7
+write /dev/cpuset/foreground/boost/cpus 4-7
+write /dev/cpuset/top-app/cpus 0-7
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
