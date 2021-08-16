48ab0c0f3f85905acc95117862c29fb831926b56
Nikko Esteves <39809545+EmperorEye1993@users.noreply.github.com>
2021-08-17 04:15:30 +0800

    Introduce: Performance Frequencies
    Add support for cpu cpufreq scaling governors and kernel's gpu governor to performance.

--- common/service.sh
+++ common/service.sh
@@ -6,11 +6,17 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/policy0/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/policy4/scaling_governor performance
 write /sys/devices/system/cpu/cpufreq/performance/above_hispeed_delay 0
 write /sys/devices/system/cpu/cpufreq/performance/boost 1
 write /sys/devices/system/cpu/cpufreq/performance/go_hispeed_load 75
 write /sys/devices/system/cpu/cpufreq/performance/max_freq_hysteresis 1
 write /sys/devices/system/cpu/cpufreq/performance/align_windows 1
+write /sys/kernel/gpu/gpu_governor performance
 write /sys/module/adreno_idler/parameters/adreno_idler_active 0
 write /sys/module/lazyplug/parameters/nr_possible_cores 8
 write /sys/module/msm_performance/parameters/touchboost 1
