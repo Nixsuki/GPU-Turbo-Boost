72f23b07e15de9329fde8f1bbc9703438fc19cbc
NitroVenom <82306740+NitroVenom@users.noreply.github.com>
2021-09-07 20:58:53 +0600

    Fixups for performance governor
    Update path for GPU governor, set performance governor for big core as well, remove adreno idler tweak since it only works if GPU governor is adreno msm tz

--- common/service.sh
+++ common/service.sh
@@ -6,13 +6,10 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor performance
-write /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor performance
-write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor performance
-write /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor performance
 write /sys/devices/system/cpu/cpufreq/policy0/scaling_governor performance
 write /sys/devices/system/cpu/cpufreq/policy4/scaling_governor performance
-write /sys/kernel/gpu/gpu_governor performance
-write /sys/module/adreno_idler/parameters/adreno_idler_active 0
+write /sys/class/kgsl/kgsl-3d0/governor performance
 write /sys/module/lazyplug/parameters/nr_possible_cores 8
 write /dev/cpuset/foreground/boost/cpus 4-7
 write /dev/cpuset/foreground/cpus 0-3,4-7
